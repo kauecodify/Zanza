603c09e022391ebfd726dd4d748d8304ec1d6188
kauecodify <143859403+kauecodify@users.noreply.github.com>
2026-05-24 14:45:48 -0300

    Update establishments.js

--- api/establishments.js
+++ api/establishments.js
@@ -1,6 +1,9 @@
 export const config = { runtime: 'edge' };
 import { supabase } from '../lib/supabase.js';
 
+export default async function handler(req) {
+  console.log('Node version:', process.version);
+
 function calcularDistancia(lat1, lon1, lat2, lon2) {
   const R = 6371;
   const dLat = (lat2 - lat1) * Math.PI / 180;
